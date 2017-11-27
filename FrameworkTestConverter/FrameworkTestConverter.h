9ca5c4259a42697f0d0e6a6d9a68577d29646f79
Dilermando Barbosa Jr <dilermando.barbosa@gmail.com>
2017-11-27 10:07:06 -0200

    Initial Commit

---
+++ FrameworkTestConverter/FrameworkTestConverter.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkTestConverter.h
+//  FrameworkTestConverter
+//
+//  Created by Diler Barbosa on 27/11/17.
+//  Copyright © 2017 Diler Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkTestConverter.
+FOUNDATION_EXPORT double FrameworkTestConverterVersionNumber;
+
+//! Project version string for FrameworkTestConverter.
+FOUNDATION_EXPORT const unsigned char FrameworkTestConverterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkTestConverter/PublicHeader.h>
+
+
